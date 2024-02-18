135184abe0ef440d7df9a35f2d541c74943918dc
Ben Warwick-Champion <ben.w-c@sky.com>
2024-02-18 16:37:48 +0000

    :fire: Remove unused greet method

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 #[derive(serde::Deserialize)]
 struct FormData {
     email: String,
     name: String,
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
 }
 
 async fn health_check() -> impl Responder {
@@ -24,10 +19,8 @@ async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
     .run();
