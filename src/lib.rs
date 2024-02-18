15ae3faa8ac8e368a45c9b28dd51dd8e36501b58
Ben Warwick-Champion <ben.w-c@sky.com>
2024-02-18 16:36:39 +0000

    :sparkles: Add dummy subscriptions endpoint and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -11,11 +17,16 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
