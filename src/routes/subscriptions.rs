baea1684a00e5365ff8541e53edf6c360a15f554
Ben Warwick-Champion <ben.w-c@sky.com>
2024-02-19 22:14:09 +0000

    :green_heart: Different way to get green CI :clown_face:

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,10 +2,11 @@ use actix_web::{web, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    _email: String,
-    _name: String,
+    email: String,
+    name: String,
 }
 
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    println!("email: {}, name: {}", _form.email, _form.name);
     HttpResponse::Ok().finish()
 }
