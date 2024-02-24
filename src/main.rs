dc9584d8ebcb730f4af49dc0664583a9fa597146
Ben Warwick-Champion <ben.w-c@sky.com>
2024-02-24 13:26:13 +0000

    :recycle: Switch to using a Pool for db interaction and complete subscribe implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -7,8 +7,8 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
